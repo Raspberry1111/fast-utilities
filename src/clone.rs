1d3eede0b6ed43089f2d789a2f77c4334bddbe7f
Raspberrry1111 <7sansandflowey@gmail.com>
2021-03-13 02:31:30 -0500

    Added better types for shallow and deep clone

--- src/clone.rs
+++ src/clone.rs
@@ -1,7 +1,21 @@
 ///! Shallow and deep cloning
-
 use js_sys::{ArrayBuffer, Object};
 use wasm_bindgen::prelude::*;
+
+// We need this to use generics for better types
+#[wasm_bindgen(typescript_custom_section)]
+const CLONE_FUNCTIONS: &'static str = r#"
+type CloneFn = <T>(x: T) => T; 
+
+/**
+* Clones all top level properties on an object
+*/
+export const shallowClone: CloneFn
+/**
+* Clones all properties on an object
+*/
+export const deepClone: CloneFn
+"#;
 
 #[wasm_bindgen(module = "v8")]
 extern "C" {
@@ -10,9 +24,8 @@ extern "C" {
 
 }
 
-
 /// Clones all top level properties on an object
-#[wasm_bindgen(js_name = shallowClone)]
+#[wasm_bindgen(js_name = shallowClone, skip_typescript)]
 pub fn shallow_clone(x: JsValue) -> JsValue {
     if !x.is_object() {
         return x;
@@ -21,7 +34,7 @@ pub fn shallow_clone(x: JsValue) -> JsValue {
 }
 
 /// Clones deeply. Requires v8 package to be present
-#[wasm_bindgen(js_name = deepClone)]
+#[wasm_bindgen(js_name = deepClone, skip_typescript)]
 pub fn deep_clone(x: JsValue) -> JsValue {
     if !x.is_object() {
         return x;
