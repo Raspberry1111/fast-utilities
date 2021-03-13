3ab8064c47e095ee848490611a596eafef2cba7c
Raspberrry1111 <7sansandflowey@gmail.com>
2021-03-13 03:07:38 -0500

    Made code more readable

--- src/clone.rs
+++ src/clone.rs
@@ -5,7 +5,7 @@ use wasm_bindgen::prelude::*;
 // We need this to use generics for better types
 #[wasm_bindgen(typescript_custom_section)]
 const CLONE_FUNCTIONS: &'static str = r#"
-type CloneFn = <T>(x: T) => T; 
+type CloneFn = <T>(object: T) => T; 
 
 /**
 * Clones all top level properties on an object
@@ -19,25 +19,27 @@ export const deepClone: CloneFn
 
 #[wasm_bindgen(module = "v8")]
 extern "C" {
-    fn serialize(obj: &JsValue) -> ArrayBuffer;
-    fn deserialize(obj: ArrayBuffer) -> JsValue;
+    fn serialize(object: &JsValue) -> ArrayBuffer;
+    fn deserialize(buffer: ArrayBuffer) -> JsValue;
 
 }
 
 /// Clones all top level properties on an object
 #[wasm_bindgen(js_name = shallowClone, skip_typescript)]
-pub fn shallow_clone(x: JsValue) -> JsValue {
-    if !x.is_object() {
-        return x;
-    }
-    Object::assign(&Object::new(), &Object::from(x)).into()
+pub fn shallow_clone(value: JsValue) -> JsValue {
+    if !value.is_object() {
+        return value;
+    };
+    let object = Object::from(value);
+    Object::assign(&Object::new(), &object).into()
 }
 
 /// Clones deeply. Requires v8 package to be present
 #[wasm_bindgen(js_name = deepClone, skip_typescript)]
-pub fn deep_clone(x: JsValue) -> JsValue {
-    if !x.is_object() {
-        return x;
-    }
-    deserialize(serialize(&Object::from(x)))
+pub fn deep_clone(value: JsValue) -> JsValue {
+    if !value.is_object() {
+        return value;
+    };
+    let object = Object::from(value);
+    deserialize(serialize(&object))
 }
